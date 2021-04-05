f16206871a0a00f82d544796b107442c2fe0e87f
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-04-05 09:28:46 +0000

    updated github-information.js as per instructions in CI video

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,21 @@
+
+function userInformationHTML(user) {
+    return `<h2>${user.name}
+    <span class ="small-name">
+    (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+    </span></h2>
+
+    <div class ="gh-content">
+        <div class ="gh-avatar"><a href="${user.html_url}" target="_blank">
+        <img src = "${user.avatar_url}" width ="80" height ="80" alt = "${user.login}"></a> 
+        </div>
+        <p> Followers: ${user.followers} - Following ${user.following}</p> <br>
+        Repos: ${user.public_repos}
+    </div>`
+}
+
+
+
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
@@ -26,5 +44,5 @@ function fetchGitHubInformation(event) {
                 )
 
             }
-        })
+        });
 }
