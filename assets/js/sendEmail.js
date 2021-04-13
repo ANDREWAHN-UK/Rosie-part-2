1c85e0e93704195a2c3f648f9bec83c982148889
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-04-13 17:39:09 +0000

    updating stuff as per lesson

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,18 @@
-console.log(hello);
+
+
+function sendMail(contactForm) {
+    emailjs.send("gmail","contact_form", {
+        
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request":contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("success ", response);
+        },
+        function(error){
+            console.log("failed", error);
+        }
+        )
+}
